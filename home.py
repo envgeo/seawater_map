69058bebb1e69380895d0473c8ad381348cb1475
envgeo <130520553+envgeo@users.noreply.github.com>
2024-05-15 13:58:34 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -101,7 +101,7 @@ def main():
         st.subheader('#01 Kodama et al., 2024.')
         #st.write('Kodama, T., Kitajima, S., Takahashi, M., and Ishimura T. (2024) Spatiotemporal variations of seawater δ18O and δD in the Western North Pacific marginal seas near Japan. Geochemical Journal. DOI: 10.2343/geochemj.GJ24009')
         st.markdown(
-            """<a style='display: block; text-align: left;' href="https://www.jstage.jst.go.jp/article/geochemj/advpub/0/advpub_GJ24009/_article">Kodama, T., Kitajima, S., Takahashi, M., and Ishimura T. (2024) Spatiotemporal variations of seawater δ18O and δD in the Western North Pacific marginal seas near Japan. Geochemical Journal. DOI: 10.2343/geochemj.GJ24009</a>
+            """<a style='display: block; text-align: left;' href="https://doi.org/10.2343/geochemj.GJ24009">Kodama, T., Kitajima, S., Takahashi, M., and Ishimura T. (2024) Spatiotemporal variations of seawater δ18O and δD in the Western North Pacific marginal seas near Japan. Geochemical Journal. DOI: 10.2343/geochemj.GJ24009</a>
             """,
             unsafe_allow_html=True,
         )
