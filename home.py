1ff712899900bb88d7583ae035b8616ed87ed299
envgeo <130520553+envgeo@users.noreply.github.com>
2024-10-08 14:01:29 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -17,13 +17,14 @@ st.set_page_config(
     # layout="wide", 
     initial_sidebar_state="auto", 
     menu_items={
-         'Get Help': 'https://www.google.com',
+         'Get Help': 'https://envgeo.h.kyoto-u.ac.jp/sw_jpn/',
          'Report a bug': "https://www.h.kyoto-u.ac.jp/academic_f/faculty_f/ishimura_toyoho_4dea/#mailform",
          'About': """
          # 日本周辺海水同位体DB
          テスト公開中
          """
      })
+
 
 
 # #セレクトボックスのリストを作成
@@ -48,22 +49,28 @@ def main():
     #タブページ
     #https://welovepython.net/streamlit-layout-container/
     st.title('SEAWATER DATA AROUND JAPAN')
-    st.write('b03-202305')
+    st.write('Version b05-202410')
+    st.write(':red[NEW!!] 2024/10/7 added data from other references in the section of "including data from other papers"')
+    # st.write('<span style="color:red;background:white">NEW!!</span> NEW',unsafe_allow_html=True)
+    # st.write("This is :blue[test]")
+    st.warning('This site is not able to support multiple simultaneous accesses, so if the display does not work, please reload the page or try again after awhile.')
         
-    tab1, tab2, tab3, tab4 = st.tabs(["main", "about", "manual", "update"])
+
+        
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["main", "about", "manual", "update", "note in Japanese"])
     
     with tab1:
         # st.header("SEAWATER DATA JAPAN (b02)")
         # st.title('SEAWATER DATA JAPAN (b02)')
 
     
-        video_file = open('data/d18O_all.mp4', 'rb')
-        video_bytes = video_file.read()
-    
-        st.video(video_bytes)
+        #video_file = open('data/d18O_all.mp4', 'rb')
+        #video_bytes = video_file.read()
+        #st.video(video_bytes)
         
+        st.video('data/d18O_all.mp4',format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=True)
     
-        st.markdown("<h6 style='text-align: center; color: grey;'>movie output by GMT(The Generic Mapping Tools)</h6>", unsafe_allow_html=True)
+        st.markdown("<h6 style='text-align: center; color: grey;'>movie outputed by GMT (The Generic Mapping Tools)</h6>", unsafe_allow_html=True)
 
 
         #画像をセンタリングするためcolumnを分ける
@@ -87,9 +94,21 @@ def main():
         st.header("about")
         # st.header('by TOYOHO ISHIMURA')
         # 純粋なテキスト
-        st.text('original data: xxx in prep.')
-        # 純粋なテキスト
         st.text('visualized by TOYOHO ISHIMURA (Python with Streamlit)')
+        
+        st.link_button("For more information and permissions for use, click here", "https://envgeo.h.kyoto-u.ac.jp/sw_jpn/")
+        
+        # 純粋なテキスト
+        st.write('_____')
+        st.header('data source')
+        st.subheader('#01 Kodama et al., 2024. (main data set)')
+        #st.write('Kodama, T., Kitajima, S., Takahashi, M., and Ishimura T. (2024) Spatiotemporal variations of seawater δ18O and δD in the Western North Pacific marginal seas near Japan. Geochemical Journal. DOI: 10.2343/geochemj.GJ24009')
+        st.markdown(
+            """<a style='display: block; text-align: left;' href="https://doi.org/10.2343/geochemj.GJ24009">Kodama, T., Kitajima, S., Takahashi, M., and Ishimura T. (2024) Spatiotemporal variations of seawater δ18O and δD in the Western North Pacific marginal seas near Japan. Geochemical Journal. DOI: 10.2343/geochemj.GJ24009</a>
+            """,
+            unsafe_allow_html=True,
+        )
+
         st.header("")
         #gifアニメを読み込む
         st.subheader('🚢 Cruise Area (2015-2021)')
@@ -99,13 +118,33 @@ def main():
         st.subheader('🚢 Sampling sites by Year')
         st.image("data/year_20230517.gif")
         
+        
+        st.write('_____')
+        
+        st.subheader('sub data set included in the section of  "including data from other papers"')
+        st.subheader('#02 Yamamoto et al., 2001.')
+        st.write('Yamamoto, M., Tanaka, N., & Tsunogai, S. (2001). Okhotsk Sea intermediate water formation deduced from oxygen isotope systematics. Journal of Geophysical Research: Oceans, 106(C12), 31075-31084.')
+        st.subheader('#03 Sakamoto et al., 2019.')
+        st.write('Sakamoto, T., Komatsu, K., Shirai, K., Higuchi, T., Ishimura, T., Setou, T., ... & Kawabata, A. (2019). Combining microvolume isotope analysis and numerical simulation to reproduce fish migration history. Methods in Ecology and Evolution, 10(1), 59-69.')
+        st.subheader('#04 Horikawa et al., 2023.')
+        st.write('Horikawa, K., Kodaira, T., Zhang, J., & Obata, H. (2023). Salinity–oxygen isotope relationship during an El Niño (2014–2015) in the southwestern Pacific and comparisons with GEOSECS data (La Niña, 1973–1974). Marine Chemistry, 249, 104222.')
+        st.subheader('#05 Kodaira et al., 2016.')
+        st.write('Kodaira,  T.,  Horikawa,  K.,  Zhang,  J. and  Senjyu,  T. (2016) Relationship between seawater oxygen isotope ratio and salinity in the Tsushima Current, the Sea of Japan. Chikyukagaku (Geochemistry). 50, 263–277. (in Japanese with English abstract) https://doi.org/10.14934/chikyukagaku.50.263')
+        st.write('')
+        
+        
+        
+        st.write('_____')
+        st.link_button("Go to Lab.", "https://envgeo.h.kyoto-u.ac.jp/sw_jpn/")
+        
+        
     with tab3:
         # st.title('SEAWATER DATA JAPAN (b03)')
 
         st.header("manual")
         st.subheader('select sub menu')
         st.write('PC operation recommended')
-        
+        st.link_button("Description of each page", "https://envgeo.h.kyoto-u.ac.jp/sw_jpn/#pages")
         
 
         
@@ -117,10 +156,37 @@ def main():
         # st.title('SEAWATER DATA JAPAN (b01)')
 
         st.header('update')
-        st.write('2023/05/22 version b03')
+        st.write('2024/09/26 added data from other references in the section of "including data from other papers"')
+        st.write('2024/07/26 minor update for all visualizer')
+        st.write('2024/05/15 opend to the public')
+        st.write('2023/07/22 bug fix')
+        st.write('2023/05/22 version b03 pre-opened')
             
 
+        # ヘッダ
+    with tab5:
+        # st.title('SEAWATER DATA JAPAN (b01)')
+
+        st.text('note by T.Ishimura')
+        st.markdown(
+            """<a style='display: block; text-align: left;' href="https://www.h.kyoto-u.ac.jp/academic_f/faculty_f/ishimura_toyoho_4dea/#mailform">問い合わせ</a>
+            """,
+            unsafe_allow_html=True,
+        )
+        st.link_button("Go to Lab.", "https://envgeo.h.kyoto-u.ac.jp/sw_jpn/")
+        
+        st.subheader('2024/05/11')
+        st.write('MacのSafariの古いバージョン(16.3以前)では表示されないようです。その他不具合・ご要望など，ご連絡いただければ幸いです。')
+        st.write('複数アクセスには対応できておりませんので，表示が上手く行かないときには時間をおいてからおためしください。')
+        st.write('今後，公表されている各種データを追加してデータの拡充をしたいと考えています。')
+
+
+
     # サブレベルヘッダ
+    
+    #色を変えるとき
+    #st.write('<span style="color:red;background:pink">該当するデータがありません・・・・</span>',unsafe_allow_html=True)
+
 
     # st.header('by TOYOHO ISHIMURA')
     # 純粋なテキスト
